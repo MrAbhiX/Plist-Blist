d5145bbcc3f8aa7be6270bbd894473f6f7dce729
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-05-29 20:47:41 +0530

    Update __main__.py

--- Abhi/__main__.py
+++ Abhi/__main__.py
@@ -11,9 +11,9 @@ os.system("pip install -U telethon")
 print(f"""MAFIABOT IS ON!!! MAFIABOT VERSION :- {mafiaversion} YOUR 𝕄𝔸𝔽𝕀𝔸𝔹𝕆𝕋 IS READY TO USE! FOR CHECK YOUR BOT WORKING OR NOT PLEASE TYPE (.alive/.ping) ENJOY YOUR BOT! JOIN FOR MORE FUTURE UPDATES @MafiaBot_Support .""")
 async def humhai():
     await bot.send_file(
-        LOGGER,caption=f"BOT IS START FOR FUCK GROUPS",
+        LOGGER, "BOT IS START FOR FUCK GROUPS",
         )
-        except Exception as e:
+    except Exception as e:
            LOGS.info(str(e))
 
 bot.loop.create_task(humhai())
