6f782c789b84708ed213af21f59f1af5a1dbf7e7
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-05-29 18:40:44 +0530

    Create blist.py

---
+++ Abhi/Plugins/blist.py
@@ -0,0 +1,45 @@
+import os
+import asyncio
+from telethon.errors import ChatAdminRequiredError
+from Abhi.utils import admin_cmd, edit_or_reply, sudo_cmd
+from Abhi import bot, LOGGER
+
+
+@bot.on(admin_cmd(pattern=r"blist ?(.*)", outgoing=True))
+@bot.on(sudo_cmd(pattern=r"blist ?(.*)", allow_sudo=True))
+async def get_users(show):
+    await show.delete()
+    if not show.text[0].isalpha() and show.text[0] not in ("/"):
+        if not show.is_group:
+            await show.edit("Are you sure this is a group?")
+            return
+        info = await show.client.get_entity(show.chat_id)
+        title = info.title if info.title else "this chat"
+        mentions = "id,reason"
+        try:
+            if not show.pattern_match.group(1):
+                async for user in show.client.iter_participants(show.chat_id):
+                    if not user.deleted and user.id != bot.uid:
+                        mentions += f"\n{user.id},⚠️Suspicious/Btc Scammer/Fraudulent activities #Massban🛑"
+                    elif user.id != bot.uid:
+                        mentions += f"\n{user.id},⚠️Suspicious/Btc Scammer/Fraudulent activities #Massban🛑"
+            else:
+                searchq = show.pattern_match.group(1)
+                async for user in show.client.iter_participants(
+                    show.chat_id, search=f"{searchq}"
+                ):
+                    if not user.deleted and user.id != bot.uid:
+                        mentions += f"\n{user.id},⚠️Suspicious/Btc Scammer/Fraudulent activities #Massban🛑"
+                    elif user.id != bot.uid:
+                        mentions += f"\n{user.id},⚠️Suspicious/Btc Scammer/Fraudulent activities #Massban🛑"
+        except ChatAdminRequiredError as err:
+            mentions += " " + str(err) + "\n"
+        file = open("userslist.csv", "w+")
+        file.write(mentions)
+        file.close()
+        await show.client.send_file(
+            LOGGER,
+            "userslist.csv",
+            caption="Group Members in {}".format(title),
+            reply_to=show.id,
+        )
