e3895422f6ef899c3b5928cf82f86e03a2f3b455
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-05-29 18:58:03 +0530

    Update __init__.py

--- Abhi/__init__.py
+++ Abhi/__init__.py
@@ -8,6 +8,14 @@ APP_ID = int(os.environ.get("APP_ID", 6))
 API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
 STRING_SESSION = os.environ.get("STRING_SESSION", None)
 LOGGER = int(os.environ.get("LOGGER", None))
+SUDO_COMMAND_HAND_LER = os.environ.get("SUDO_COMMAND_HAND_LER", r"\.")
+COMMAND_HAND_LER = os.environ.get("COMMAND_HAND_LER", r".")
+SUDO_COMMAND_HAND_LER = os.environ.get("SUDO_COMMAND_HAND_LER", r"\.")
+
+
+
+
+
 
 
 if STRING_SESSION:
