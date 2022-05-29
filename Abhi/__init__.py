6d12e693d3357d29c535b0df376beb4f3b4282fa
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-05-29 18:30:06 +0530

    Update __init__.py

--- Abhi/__init__.py
+++ Abhi/__init__.py
@@ -1,2 +1,17 @@
+import os
 from telethon.sessions import StringSession
 from telethon import TelegramClient
+
+
+APP_ID = int(os.environ.get("APP_ID", 6))
+    # 6 is a placeholder
+API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+STRING_SESSION = os.environ.get("STRING_SESSION", None)
+
+
+if STRING_SESSION:
+    session_name = str(STRING_SESSION)
+    bot = TelegramClient(StringSession(session_name), APP_ID, API_HASH)
+else:
+    session_name = "startup"
+    bot = TelegramClient(session_name, APP_ID, API_HASH)
