91e5d6a2013200de18f38909f1ea6d7ac8cefdc9
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-05-29 21:26:33 +0530

    Update __main__.py

--- Abhi/__main__.py
+++ Abhi/__main__.py
@@ -3,11 +3,41 @@ import os
 from Abhi import bot, LOGGER
 import asyncio
 from sys import argv
+from telethon import TelegramClient
+import telethon.utils
+
 
 LOGS = getLogger(__name__)
 
 os.system("pip install -U telethon")
 
+
+async def add_bot(bot_token):
+    await bot.start(bot_token)
+    bot.me = await bot.get_me() 
+    bot.uid = telethon.utils.get_peer_id(bot.me)
+
+
+
+if len(argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.tgbot = None
+    if BOT_USERNAME is not None:
+        print("Initiating Inline Bot")
+        # ForTheGreatrerGood of beautification
+        bot.tgbot = TelegramClient(
+            "TG_BOT_TOKEN",
+            api_id=APP_ID,
+            api_hash=API_HASH
+        ).start(bot_token=BOT_TOKEN)
+        print("Initialisation finished with no errors")
+        print("Starting MafiaBot")
+        bot.loop.run_until_complete(add_bot(BOT_USERNAME))
+        print("MafiaBot Startup Completed")
+    else:
+        bot.start()
+        
 
 print(f"I'M STARTED")
 async def humhai():
@@ -21,4 +51,8 @@ async def humhai():
     except Exception as e:
         LOGS.info(str(e))
 
-bot.start()
+bot.loop.create_task(mafia_is_on())
+if len(argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.run_until_disconnected()
